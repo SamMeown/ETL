f08c2b68bbfcc891fb3f0c19821996642449494e
Sam <sammeown@gmail.com>
2021-09-02 18:52:37 +0300

    Добавил админки для фильмов, жанров и персон.

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import Filmwork, FilmworkGenre, Genre, FilmworkPerson, Person
 
-# Register your models here.
+
+class GenreInline(admin.TabularInline):
+    model = FilmworkGenre
+    extra = 0
+
+
+class PersonInline(admin.TabularInline):
+    model = FilmworkPerson
+    extra = 0
+    # fields = ('role',)
+    ordering = ('-role', 'person__full_name')
+
+
+@admin.register(Filmwork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'creation_date', 'rating')
+    fields = (
+        'title', 'type', 'description', 'creation_date', 'certificate',
+        'file_path', 'rating',
+    )
+
+    list_filter = ('type',)
+
+    search_fields = ('title', 'description', 'id',)
+
+    inlines = [
+        GenreInline,
+        PersonInline,
+    ]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name', 'description')
+
+    search_fields = ('name',)
+
+    ordering = ('name',)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date')
+    fields = ('full_name', 'birth_date')
+
+    search_fields = ('full_name',)
+
+    ordering = ('full_name',)
