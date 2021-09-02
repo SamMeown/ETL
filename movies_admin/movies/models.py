e1c9ff5f9e4ae5265242caaeb6c2182e605e028b
Sam <sammeown@gmail.com>
2021-09-02 18:46:02 +0300

    Добавил в проект приложение movies. Добавил модели для всех таблиц, создал начальную миграцию.

---
+++ movies_admin/movies/models.py
@@ -0,0 +1,125 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+
+import uuid
+
+
+# Добавлям классы для текстового и файлового полей, которые пустые значения сохраняют в базу данных как NULL.
+# Делаем так потому, что в базе, которую мы унаследовали, используется такое соглашение (не пустые строки, а NULL).
+class TextNullField(models.TextField):
+    """TextField that stores NULL when empty"""
+    def get_db_prep_value(self, value, connection, prepared=False):
+        value = super().get_db_prep_value(value, connection, prepared)
+        if value == "":
+            return None
+        else:
+            return value
+
+
+class FileNullField(models.FileField):
+    """FileField that stores NULL when empty"""
+    def get_db_prep_value(self, value, connection, prepared=False):
+        value = super().get_db_prep_value(value, connection, prepared)
+        if value == "":
+            return None
+        else:
+            return value
+
+
+class TimeStampedMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(TimeStampedMixin, models.Model):
+    """Модель для жанров кинопроизведений"""
+    id = models.UUIDField(_('ID'), primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(_('title'), max_length=80)
+    description = models.CharField(_('description'), max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('genre')
+        verbose_name_plural = _('genres')
+        db_table = '"content"."genre"'
+
+
+class Person(TimeStampedMixin, models.Model):
+    """Модель для персон (актеры, режиссеры, сценаристы, ...)"""
+    id = models.UUIDField(_('ID'), primary_key=True, default=uuid.uuid4, editable=False)
+    full_name = models.CharField(_('full name'), max_length=255)
+    birth_date = models.DateField(_('birth date'), blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = _('person')
+        verbose_name_plural = _('persons')
+        db_table = '"content"."person"'
+
+
+class PersonType(models.TextChoices):
+    ACTOR = 'actor', _('actor')
+    DIRECTOR = 'director', _('director')
+    WRITER = 'writer', _('writer')
+
+
+class FilmworkPerson(models.Model):
+    """Модель для связи персон и кинопроизведений"""
+    id = models.UUIDField(_('ID'), primary_key=True, default=uuid.uuid4, editable=False)
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    role = models.CharField(_('role'), max_length=20, choices=PersonType.choices, default=PersonType.ACTOR)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = '"content"."person_film_work"'
+        unique_together = [['film_work', 'person', 'role']]
+
+
+class FilmworkGenre(models.Model):
+    """Модель для связи жанров и кинопроизведений"""
+    id = models.UUIDField(_('ID'), primary_key=True, default=uuid.uuid4, editable=False)
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # auto_created = True
+        db_table = '"content"."genre_film_work"'
+        unique_together = [['film_work', 'genre']]
+
+
+class FilmworkType(models.TextChoices):
+    MOVIE = 'movie', _('movie')
+    TV_SHOW = 'tv_show', _('tv show')
+
+
+class Filmwork(TimeStampedMixin, models.Model):
+    """Модель для кинопроизведения (фильма, телешоу, ...)"""
+    id = models.UUIDField(_('ID'), primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(_('title'), max_length=255)
+    description = TextNullField(_('description'), blank=True, null=True, default=None)
+    creation_date = models.DateField(_('creation date'), blank=True, null=True)
+    certificate = models.CharField(_('certificate'), max_length=255, blank=True, null=True)
+    file_path = FileNullField(_('file'), upload_to='film_works/', max_length=255, blank=True, null=True, default=None)
+    rating = models.FloatField(_('rating'), validators=[MinValueValidator(0), MaxValueValidator(10)],
+                               blank=True, null=True)
+    type = models.CharField(_('type'), max_length=20, choices=FilmworkType.choices, default=FilmworkType.MOVIE)
+    genres = models.ManyToManyField(Genre, through='FilmworkGenre')
+    persons = models.ManyToManyField(Person, through='FilmworkPerson')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('filmwork')
+        verbose_name_plural = _('filmworks')
+        db_table = '"content"."film_work"'
