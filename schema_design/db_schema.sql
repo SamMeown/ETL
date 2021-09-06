906f1f1e6bb13d208e7af54fead5754be83db626
Sam <sammeown@gmail.com>
2021-09-06 16:30:00 +0300

    Замечания по ревью - поправил схему, добавил индексы

--- schema_design/db_schema.sql
+++ schema_design/db_schema.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA IF NOT EXISTS content;
 
 -- Жанры кинопроизведений:
 CREATE TABLE IF NOT EXISTS content.genre (
-  id uuid PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR(80) NOT NULL,
   description VARCHAR(255),
   created_at timestamp with time zone,
@@ -12,17 +12,18 @@ CREATE TABLE IF NOT EXISTS content.genre (
 
 -- Персоны (актеры, режиссеры, сценаристы)
 CREATE TABLE IF NOT EXISTS content.person (
-  id uuid PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   full_name VARCHAR(255) NOT NULL,
   birth_date DATE,
   created_at timestamp with time zone,
   updated_at timestamp with time zone
 );
+CREATE INDEX person_full_name_idx ON content.person(full_name);
 
 -- Фильмы
 CREATE TYPE content.film_work_type AS ENUM ('movie', 'tv_show');
 CREATE TABLE IF NOT EXISTS content.film_work (
-  id uuid PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   title VARCHAR(255) NOT NULL,
   description TEXT,
   creation_date DATE,
@@ -33,37 +34,26 @@ CREATE TABLE IF NOT EXISTS content.film_work (
   created_at timestamp with time zone,
   updated_at timestamp with time zone
 );
+CREATE INDEX film_work_title_idx ON content.film_work(title);
+CREATE INDEX film_work_creation_date_idx ON content.film_work(creation_date);
 
 -- Жанры фильмов
 CREATE TABLE IF NOT EXISTS content.genre_film_work (
-  id uuid PRIMARY KEY,
-  film_work_id uuid NOT NULL,
-  genre_id uuid NOT NULL,
-  created_at timestamp with time zone,
-
-  FOREIGN KEY (film_work_id) REFERENCES content.film_work(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  FOREIGN KEY (genre_id) REFERENCES content.genre(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  film_work_id uuid NOT NULL REFERENCES content.film_work(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  genre_id uuid NOT NULL REFERENCES content.genre(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  created_at timestamp with time zone
 );
 CREATE UNIQUE INDEX genre_film_work_idx ON content.genre_film_work (film_work_id, genre_id);
 
 -- Персоны в фильмах
 CREATE TYPE content.person_film_work_role AS ENUM ('actor', 'director', 'writer');
 CREATE TABLE IF NOT EXISTS content.person_film_work (
-  id uuid PRIMARY KEY,
-  film_work_id uuid NOT NULL,
-  person_id uuid NOT NULL,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  film_work_id uuid NOT NULL REFERENCES content.film_work(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  person_id uuid NOT NULL REFERENCES content.person(id) ON DELETE CASCADE ON UPDATE CASCADE,
   role content.person_film_work_role NOT NULL,
-  created_at timestamp with time zone,
-
-  FOREIGN KEY (film_work_id) REFERENCES content.film_work(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  FOREIGN KEY (person_id) REFERENCES content.person(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+  created_at timestamp with time zone
 );
 CREATE UNIQUE INDEX person_film_work_idx ON content.person_film_work (film_work_id, person_id, role);
+CREATE INDEX person_film_work_person_idx ON content.person_film_work(person_id);
