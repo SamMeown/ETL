a66a498bbd453eb99436bd966b7c4b3ac8e85fde
Sam <sammeown@gmail.com>
2021-09-20 19:17:15 +0300

    Добавил django_extensions для удобной консоли shell_plus

--- movies_admin/config/settings/dev.py
+++ movies_admin/config/settings/dev.py
@@ -5,6 +5,9 @@ from .base import *
 
 DEBUG = True
 
+# Для удобной консоли (shell_plus) и пр.
+INSTALLED_APPS += ['django_extensions']
+
 # Настройки для debug_toolbar
 USE_DEBUG_TOOLBAR = False
 if USE_DEBUG_TOOLBAR:
